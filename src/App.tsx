6198ba6fcc204c7249bc898e796c86c575756d82
MrWilllian <willian.dev10@gmail.com>
2020-08-11 18:58:56 -0300

    refactor: add Introduction page on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
+import Introduction from './pages/Introduction';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <GlobalStyles />
+
+      <Header />
+
+      <Introduction />
+      
     </>
   );
 }
