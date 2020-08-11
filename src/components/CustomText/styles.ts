903e3a10a09936bb9691d53cb3466116a79d3496
MrWilllian <willian.dev10@gmail.com>
2020-08-11 18:52:55 -0300

    style: add Custom Text component style

---
+++ src/components/CustomText/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+interface TextProps {
+  weight?: number;
+  size?: number;
+  isWhiteText?: boolean;
+}
+
+export const Text = styled.h1<TextProps>`
+  color: ${props => props.isWhiteText 
+          ? '#FFF' : '#666874'};
+  font-weight: ${props => props.weight};
+  font-size: ${props => props.size}px;
+`;
