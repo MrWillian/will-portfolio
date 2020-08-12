fb21358cd94a5fd878aadf27ec35a5bf3ef68f3b
MrWilllian <willian.dev10@gmail.com>
2020-08-12 14:39:48 -0300

    refactor: add Project component

---
+++ src/components/Project/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 3rem;
+  min-width: 60vw;
+  min-height: 50vh;
+
+  background-color: var(--grey2);
+`;
