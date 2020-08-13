1d608c115027ac6046d66079391f19f3b0a185e0
MrWilllian <willian.dev10@gmail.com>
2020-08-13 16:21:30 -0300

    style: adjust project style

--- src/components/Project/styles.ts
+++ src/components/Project/styles.ts
@@ -2,8 +2,17 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin-top: 3rem;
-  min-width: 60vw;
+  min-width: 30vw;
   min-height: 50vh;
 
   background-color: var(--grey2);
+  border-radius: 0.5rem;
+  -webkit-box-shadow: 4px 7px 10px 1px rgba(0,0,0,0.75);
+  -moz-box-shadow: 4px 7px 10px 1px rgba(0,0,0,0.75);
+  box-shadow: 4px 7px 10px 1px rgba(0,0,0,0.75);
+
+  @media(min-width: 800px) {
+    margin-top: 0;
+    margin-bottom: 1rem;
+  }
 `;
