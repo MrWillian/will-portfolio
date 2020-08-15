496b1b40455f4be8a59f090a3e923ecdcbd7abd4
MrWilllian <willian.dev10@gmail.com>
2020-08-15 12:58:36 -0300

    refactor: add Explore More page

---
+++ src/pages/ExploreMore/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import BackButton from '../../components/BackButton';
+import CustomText from '../../components/CustomText';
+import ProjectViewer from '../../components/ProjectViewer';
+
+import { 
+  Container, ExploreMoreHeader, TitleContainer, Line,
+  ProjectsContainer, Categories, Category
+} from './styles';
+
+const ExploreMore: React.FC = () => {
+  return (
+    <Container>
+      <BackButton />
+
+      <ExploreMoreHeader>
+        <TitleContainer>
+
+          <Line />
+
+          <CustomText
+            text="Projetos"
+            isWhiteText={false} 
+            size={1.5} />
+          
+        </TitleContainer>
+
+        <CustomText 
+          text="Alguns dos meus projetos já desenvolvidos:"
+          weight={600}
+          size={0.8} />
+      </ExploreMoreHeader>
+
+      <ProjectsContainer>
+        <Categories>
+          <Category>Todos</Category>
+          <Category>Web</Category>
+          <Category>Mobile</Category>
+        </Categories>
+        
+        <ProjectViewer title="Landing Page 1" />
+        <ProjectViewer title="Site 1" />
+        <ProjectViewer title="Site 2" />
+        <ProjectViewer title="App 1" />
+      </ProjectsContainer>
+
+    </Container>
+  );
+}
+
+export default ExploreMore;
