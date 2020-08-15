f960b85c9755c9c5a75f7744ecb26ab1ee8c9539
MrWilllian <willian.dev10@gmail.com>
2020-08-15 12:57:40 -0300

    refactor: add Back Button component

---
+++ src/components/BackButton/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  display: flex;
+
+  color: var(--accent-color);
+  background-color: transparent;
+  border: none;
+  outline: 0;
+`;
+
+export const Text = styled.h3`
+  font-size: 1rem;
+`;
