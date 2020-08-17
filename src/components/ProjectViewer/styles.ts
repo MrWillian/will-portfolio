ff75647edeb75ec28cb0db2aff2464ce062ae600
MrWilllian <willian.dev10@gmail.com>
2020-08-17 09:32:31 -0300

    style: Project Viewer style changes

--- src/components/ProjectViewer/styles.ts
+++ src/components/ProjectViewer/styles.ts
@@ -8,11 +8,28 @@ export const Viewer = styled.div`
   min-width: 10rem;
   min-height: 12rem;
   background-color: var(--grey1);
-  border-radius: 0.2rem;
+  border-radius: 0.3rem;
 
   -webkit-box-shadow: 3px 4px 7px 0px rgba(0,0,0,0.75);
   -moz-box-shadow: 3px 4px 7px 0px rgba(0,0,0,0.75);
   box-shadow: 3px 4px 7px 0px rgba(0,0,0,0.75);
+
+  > img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    border-radius: 0.3rem;
+  }
+
+  &:hover {
+    -webkit-box-shadow: 1px 2px 4px 0px var(--accent-color);
+    -moz-box-shadow: 1px 2px 4px 0px var(--accent-color);
+    box-shadow: 1px 2px 4px 0px var(--accent-color);
+
+    > img {
+      opacity: 0.8;
+    }
+  }
 `;
 
 export const InfoContainer = styled.div`
