74014d05d4e560d1d16f726b1a9d8e909e345a4a
MrWilllian <willian.dev10@gmail.com>
2020-08-18 08:02:02 -0300

    refactor: last adjusts in Project Viewer component

--- src/components/ProjectViewer/styles.ts
+++ src/components/ProjectViewer/styles.ts
@@ -1,25 +1,27 @@
 import styled from 'styled-components';
 
+interface ViewerProps {
+  image: string;
+}
+
 export const Container = styled.div`
   margin: 1rem;
 `;
 
-export const Viewer = styled.div`
+export const Viewer = styled.div<ViewerProps>`
   min-width: 10rem;
   min-height: 12rem;
   background-color: var(--grey1);
   border-radius: 0.3rem;
 
+  background-image: url(${props => props.image});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
   -webkit-box-shadow: 3px 4px 7px 0px rgba(0,0,0,0.75);
   -moz-box-shadow: 3px 4px 7px 0px rgba(0,0,0,0.75);
   box-shadow: 3px 4px 7px 0px rgba(0,0,0,0.75);
-
-  > img {
-    display: block;
-    max-width: 100%;
-    height: auto;
-    border-radius: 0.3rem;
-  }
 
   &:hover {
     -webkit-box-shadow: 1px 2px 4px 0px var(--accent-color);
