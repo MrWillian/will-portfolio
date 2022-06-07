180c5bcb33e359360082b084f0eabbfdec9a48d6
MrWillian <willian.dev10@gmail.com>
2022-06-06 21:19:23 -0300

    refactor: change Explore page to get projects from json file

--- src/pages/ExploreMore/index.tsx
+++ src/pages/ExploreMore/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BackButton from '../../components/BackButton';
 import CustomText from '../../components/CustomText';
 import ProjectViewer from '../../components/ProjectViewer';
@@ -7,13 +7,28 @@ import TwitterClone from '../../assets/images/twitter-clone.jpg';
 import DiscordClone from '../../assets/images/discord-clone.jpg';
 import CovidTracker from '../../assets/images/covid-tracker.jpg';
 import TaskManager from '../../assets/images/task-manager.jpg';
+import projectsJson from '../../assets/data/projects.json';
 
 import { 
-  Container, ExploreMoreHeader, TitleContainer, Line,
-  ProjectsContainer, Projects, Categories, Category
+  Container, ExploreMoreHeader, TitleContainer, Line, ProjectsContainer, Projects, Categories, Category
 } from './styles';
 
+// interface Projects {
+//   projects: [
+//     {
+//       title: string,
+//       description: string,
+//       link: string,
+//       image: string  
+//     }
+//   ]
+// }
+
 const ExploreMore: React.FC = () => {
+  // const items = useState<Projects[]>(projectsJson);
+
+  console.log("items", projectsJson);
+
   return (
     <Container>
       <BackButton />
@@ -45,25 +60,13 @@ const ExploreMore: React.FC = () => {
         
         <Projects>
 
-          <ProjectViewer
-            title="Twitter Clone" 
-            link="https://willian-twitter-clone.netlify.app"
-            src={TwitterClone} />
-          
-          <ProjectViewer
-            title="Discord Clone" 
-            link="https://willian-discord-clone.netlify.app"
-            src={DiscordClone} />
-          
-          <ProjectViewer
-            title="Covid Tracker" 
-            link="https://covid-world-tracker.netlify.app/"
-            src={CovidTracker} />
-          
-          <ProjectViewer
-            title="Task Manager" 
-            link="https://task-manager-willian.netlify.app/"
-            src={TaskManager} />
+          {projectsJson.projects.map((project, index) => (
+            <ProjectViewer
+              title={project.title}
+              link={project.link}
+              src={project.image} 
+              key={index} />
+          ))}
           
         </Projects>
       </ProjectsContainer>
