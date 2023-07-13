1d16853ac0e9828ef1fd8b703ee315cc076c1a38
MrWillian <willian.dev10@gmail.com>
2023-07-13 11:23:13 -0300

    feat(component): translate Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import MenuItem from '../MenuItem';
+import { useTranslation } from 'react-i18next';
 
 import { Container } from './styles';
 
 const Menu: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <Container>
-      <MenuItem title="Introdução" link="introduction" isActive />
-      <MenuItem title="Contato" link="contact" />
-      <MenuItem title="Trabalhos" link="portfolio" />
+      <MenuItem title={t('menu.introduction')} link="introduction" isActive />
+      <MenuItem title={t('menu.contact')} link="contact" />
+      <MenuItem title={t('menu.jobs')} link="portfolio" />
     </Container>
   );
 }
